6c1b372ea18d1685ead93d4d8680ab6bb94fbee7
Achelov efim <mobster-qg@@mail.ru>
2022-12-08 03:29:07 +0700

    dz29

---
+++ dz29/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+
+int[] array = mass(8);
+Console.WriteLine($"[{String.Join(",", array)}]");
+
+
+int[] mass(int size)
+{
+    int[] result = new int  [size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(7);
+    }
+
+    return result;
+}
