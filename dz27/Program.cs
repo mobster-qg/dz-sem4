e48efc29f490d42eb305c05b461459c5d65c1f4b
Achelov efim <mobster-qg@@mail.ru>
2022-12-08 03:29:44 +0700

    dz27

---
+++ dz27/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+Console.Clear();
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"сумма = {GetMultiply(num)}");
+
+
+int GetMultiply(int number)
+{
+    int result = 0;
+    for (int i = 0; i <= number; i++)
+    {
+        result = i % 10 +i / 10 % 10 + i / 100;
+       
+    }
+    return result;
+}
